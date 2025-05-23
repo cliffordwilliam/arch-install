eb801a7d2de985cc27983bb3dbfc3689676bc36b
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-05-23 23:35:15 +0700

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -29,15 +29,17 @@ for repo in "${REPOS[@]}"; do
   sudo -u "$TARGET_USER" bash -c "
     cd $BUILD_DIR
     git clone $URL_BASE/$repo
-    cd $repo
-    make clean install
   "
+  cd "$BUILD_DIR/$repo"
+  sudo -u "$TARGET_USER" make clean
+  make install
   rm -rf "$BUILD_DIR/$repo"
 done
 
 echo "=== Creating .xinitrc to launch dwm ==="
 echo "exec dwm" > "$USER_HOME/.xinitrc"
 chown "$TARGET_USER:$TARGET_USER" "$USER_HOME/.xinitrc"
+chmod +x "$USER_HOME/.xinitrc"
 
 echo "=== Cleaning up ==="
 rmdir "$BUILD_DIR"
