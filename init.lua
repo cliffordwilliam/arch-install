4fbcc1559af85cf694d7f3d85ff858c3f6eb6e92
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-10-26 19:03:37 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,19 +7,14 @@ end)
 vim.opt.number = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.hlsearch = false
 vim.opt.wrap = false
-vim.opt.breakindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", lazypath })
   if vim.v.shell_error ~= 0 then
     error("Error cloning lazy.nvim:\n" .. out)
   end
@@ -35,10 +30,8 @@ require("lazy").setup({
     },
     config = function()
       require("telescope").setup({})
-
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
     end,
   },
 
@@ -47,7 +40,6 @@ require("lazy").setup({
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     opts = {
-      ensure_installed = { "lua", "python", "bash", "typescript", "javascript" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
@@ -63,22 +55,9 @@ require("lazy").setup({
     },
     config = function()
       vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+        group = vim.api.nvim_create_augroup("my-lsp", { clear = true }),
         callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or "n"
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-
-          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { buffer = event.buf })
         end,
       })
 
@@ -88,14 +67,15 @@ require("lazy").setup({
       local servers = {
         pylsp = {},
         ts_ls = {},
+        clangd = {},
       }
 
       require("mason-lspconfig").setup({
-        ensure_installed = vim.tbl_keys(servers or {}),
+        ensure_installed = vim.tbl_keys(servers),
         handlers = {
           function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            local server = servers[server_name]
+            server.capabilities = capabilities
             require("lspconfig")[server_name].setup(server)
           end,
         },
@@ -139,17 +119,6 @@ require("lazy").setup({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete({}),
-          ["<C-l>"] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { "i", "s" }),
-          ["<C-h>"] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { "i", "s" }),
         }),
         sources = {
           { name = "nvim_lsp" },
