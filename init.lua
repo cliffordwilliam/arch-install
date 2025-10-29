2c8cb8e7ec7ef57d936f3409c5583bc03b5c947f
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-10-29 21:38:38 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -73,8 +73,6 @@ require("lazy").setup({
         },
         completion = { completeopt = "menu,menuone,noinsert" },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
