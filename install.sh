2d3a29c7d962aae8f961444548bdfce9b47495b0
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-28 21:28:19 +0700

    Add arandr to package installation list

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ systemctl enable NetworkManager
 pacman -Syu --noconfirm
 
 pacman -S --noconfirm base-devel xorg xorg-xinit i3 i3status dmenu \
-    kitty qutebrowser alsa-utils ufw git github-cli tmux \
+    kitty qutebrowser alsa-utils ufw git github-cli tmux arandr \
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig
 
 systemctl enable ufw
