e1927e46b4128f7a82ef86a85340086424348353
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-11-29 09:22:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ systemctl enable NetworkManager
 pacman -Syu --noconfirm
 
 pacman -S --noconfirm base-devel xorg xorg-xinit i3 i3status dmenu \
-    ghostty qutebrowser alsa-utils ufw git github-cli tmux arandr \
+    kitty qutebrowser alsa-utils ufw git github-cli tmux \
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig
 
 systemctl enable ufw
