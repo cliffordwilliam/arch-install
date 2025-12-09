6bb1b7691444b5072def5ebec082a905286db5b1
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2025-12-10 05:46:42 +0700

    Remove kitty from install script dependencies

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ systemctl enable NetworkManager
 pacman -Syu --noconfirm
 
 pacman -S --noconfirm base-devel xorg xorg-xinit i3 i3status dmenu \
-    kitty qutebrowser alsa-utils ufw git github-cli tmux \
+    ghostty qutebrowser alsa-utils ufw git github-cli \
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig
 
 systemctl enable ufw
